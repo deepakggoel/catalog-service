cdf9395be28a64d9a761d91da6b671a210f36054
Deepak Goel <deepak@agamin.biz>
2025-03-09 18:22:42 -0700

    Revert "revert to Spring Boot 2.7"
    This reverts commit 56cd01173e66db4a04cedf4db6846ed7d00c9382.

--- src/main/java/com/polarbookshop/catalogservice/config/DataConfig.java
+++ src/main/java/com/polarbookshop/catalogservice/config/DataConfig.java
@@ -1,9 +1,6 @@
 package com.polarbookshop.catalogservice.config;
 
-import com.polarbookshop.catalogservice.domain.Book;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
 
 @Configuration
